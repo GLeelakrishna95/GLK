c3e4067a3ddd21ab09ceb256dcf2896de150b81d
GLeelakrishna95 <154605397+GLeelakrishna95@users.noreply.github.com>
2023-12-22 18:42:25 +0530

    Add files via upload

---
+++ coursera.py
@@ -0,0 +1,79 @@
+Exercise 2: Create a markdown cell with the title of the notebook. (1 pt)
+
+markdown
+Copy code
+# My Data Science Notebook
+Exercise 3: Create a markdown cell for an introduction. (1 pt)
+
+markdown
+Copy code
+## Introduction
+Welcome to my data science notebook. In this document, I explore various data science tools and concepts using Jupyterlite Notebook.
+Exercise 4: Create a markdown cell to list data science languages. (3 pts)
+
+markdown
+Copy code
+## Data Science Languages
+1. Python
+2. R
+3. SQL
+Exercise 5: Create a markdown cell to list data science libraries. (3 pts)
+
+markdown
+Copy code
+## Data Science Libraries
+- NumPy
+- Pandas
+- Matplotlib
+- Seaborn
+- Scikit-learn
+Exercise 6: Create a markdown cell with a table of Data Science tools. (3 pts)
+
+markdown
+Copy code
+## Data Science Tools
+
+| Tool          | Purpose                 |
+|---------------|-------------------------|
+| Jupyter       | Interactive Computing   |
+| VS Code       | Integrated Development  |
+| Tableau       | Data Visualization      |
+| Docker        | Containerization        |
+Exercise 7: Create a markdown cell introducing arithmetic expression examples. (1 pt)
+
+markdown
+Copy code
+## Arithmetic Expression Examples
+Let's explore some basic arithmetic operations using Python.
+Exercise 8: Create a code cell to multiply and add numbers. (2 pts)
+
+python
+Copy code
+# Multiply and add numbers
+result_multiply = 5 * 10
+result_add = 5 + 10
+
+result_multiply, result_add
+Exercise 9: Create a code cell to convert minutes to hours. (2 pts)
+
+python
+Copy code
+# Convert minutes to hours
+minutes = 120
+hours = minutes / 60
+
+hours
+Exercise 10: Insert a markdown cell to list Objectives. (3 pts)
+
+markdown
+Copy code
+## Objectives
+1. Explore data science languages and libraries.
+2. Perform basic arithmetic operations using Python.
+3. Understand the conversion of minutes to hours.
+Exercise 11: Create a markdown cell to indicate the Author’s name. (2 pts)
+
+markdown
+Copy code
+## Author
+John Doe
